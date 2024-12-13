5f78bdf33ffb8e8cfb4a96a90051eb05d02fe5a6
pricope-marius-andrei <pricopemarius33@gmail.com>
2024-12-13 23:19:35 +0200

    add the subreddit name, description, subscribers and active users in a web page

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Reddit-Python
+
+- [x] numele subreddit-ului
+- [x] descriere
+- [x] subscribers
+- [x] active users
+- [x] thread uri highlightate (afisati metadatele thread-ului, precum si un link catre acesta)
+- [x] numarul de postari din ultimele 24h
+- [x] o statistica cu cele mai folosite label-uluri (flairs) in ultimele X thread-uri hot (sort by hot)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta, timezone
 from dotenv import load_dotenv
 
 from flask import Flask, render_template
@@ -9,31 +10,60 @@ app = Flask(__name__)
 
 load_dotenv()
 
+subreddit_name = "Amd"
+threads_limit = 100
+
 reddit = Reddit()
 
 reddit.generate_token()
 
-about = reddit.get_call("/r/EASportsFC/about", "")
+
+about = reddit.get_call(f"/r/{subreddit_name}/about", "")
 
 about_data = about["data"]
 
+data = reddit.get_call(f"/r/{subreddit_name}/hot", f"?g=GLOBAL&count=0&limit={threads_limit}")["data"]["children"]
 
-# data = reddit.get_call("/r/EASportsFC/top", "?g=GLOBAL&count=0&limit=25")["data"]
+flairs_freq = {}
 
-data = reddit.get_call("/r/EASportsFC/top", "?t=day&count=0&limit=26")
+for subreddit in data:
+    flair = subreddit["data"]["author_flair_text"]
+    if flair is not None:
+        if flair in flairs_freq:
+            flairs_freq[flair] += 1
+        else:
+            flairs_freq[flair] = 1
 
-print(data)
+flairs_freq = dict(sorted(flairs_freq.items(), key=lambda x: x[1], reverse=True))
 
-# for post in data["children"]:
-#     print(post["data"]["url"])
+new_posts = reddit.get_call(f"/r/{subreddit_name}/new", "?count=0&limit=100")["data"]["children"]
 
+last_24_hours_posts = 0
+for new_post in new_posts:
+    created_time = new_post["data"]["created_utc"]
+    if created_time > (datetime.now(timezone.utc) - timedelta(days=1)).timestamp():
+        last_24_hours_posts += 1
+
+print(last_24_hours_posts)
+
+
+posts = reddit.get_call(f"/r/{subreddit_name}/hot", "?count=0&limit=10")["data"]["children"]
+
+print(posts)
+
+for post in posts:
+    print(post["data"]["title"])
 @app.route('/')
 def home():
     return render_template('index.html',
                            display_name=about_data["display_name"],
                            accounts_active=about_data["accounts_active"],
                            subscribers=about_data["subscribers"],
-                           description=about_data["public_description"])
+                           description=about_data["public_description"],
+                           threads_limit=threads_limit,
+                           flair_freq_list=flairs_freq.items(),
+                           last_24_hours_posts=last_24_hours_posts,
+                           threads_list=posts)
 
 
 if __name__ == '__main__':
